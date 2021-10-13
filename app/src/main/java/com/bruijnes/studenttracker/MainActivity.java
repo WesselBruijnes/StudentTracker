87a5ad28aac772f82459240580ba502efdc59501
wessel <wessel@taxibutler.com>
2021-10-13 22:31:01 +0200

    Added studentRecycleView

--- app/src/main/java/com/bruijnes/studenttracker/MainActivity.java
+++ app/src/main/java/com/bruijnes/studenttracker/MainActivity.java
@@ -3,7 +3,6 @@ package com.bruijnes.studenttracker;
 import static android.content.ContentValues.TAG;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
--- app/src/main/java/com/bruijnes/studenttracker/StudentActivity.java
+++ app/src/main/java/com/bruijnes/studenttracker/StudentActivity.java
@@ -1,8 +1,19 @@
 package com.bruijnes.studenttracker;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bruijnes.studenttracker.adapter.StudentAdapter;
+import com.bruijnes.studenttracker.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentActivity extends AppCompatActivity {
 
@@ -11,7 +22,19 @@ public class StudentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
 
-
+        List<Student> students = new ArrayList<>();
+        for(int i = 0; i < 20; i++) {
+            Student x = new Student();
+            x.setStudentId(i);
+            x.setFirstName("wessel");
+            x.setLastName("bruijnes");
+            students.add(x);
+        }
+        students.forEach(student1 -> Log.d(TAG, "onCreate: " + student1.toString()));
+        StudentAdapter studentAdapter = new StudentAdapter(this, students);
+        RecyclerView view = findViewById(R.id.studentRecycleView);
+        view.setAdapter(studentAdapter);
+        view.setLayoutManager(new LinearLayoutManager(this));
 
     }
 }
