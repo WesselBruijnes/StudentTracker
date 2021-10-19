5dd367bc89873061299fb4b9c01475b35606c737
wessel <wessel@taxibutler.com>
2021-10-19 19:13:47 +0200

    Add student activity

--- app/src/main/java/com/bruijnes/studenttracker/AddStudentActivity.java
+++ app/src/main/java/com/bruijnes/studenttracker/AddStudentActivity.java
@@ -8,12 +8,15 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bruijnes.studenttracker.exception.StudentTrackerException;
 import com.bruijnes.studenttracker.model.Student;
+import com.google.android.gms.common.util.Strings;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class AddStudentActivity extends AppCompatActivity {
 
@@ -34,7 +37,7 @@ public class AddStudentActivity extends AppCompatActivity {
 
         findViewById(R.id.saveButton).setOnClickListener(view -> {
             try {
-
+                validateFields();
                 Student student = Student.builder()
                         .firstName(firstName.getText().toString())
                         .lastName(lastName.getText().toString())
@@ -43,18 +46,39 @@ public class AddStudentActivity extends AppCompatActivity {
                         .createdAt(ZonedDateTime.now())
                         .build();
             Snackbar.make(view, student.toString(), 2000).show();
+            super.finish();
             // TODO: Save to firebase
+            } catch (StudentTrackerException e) {
+                Snackbar.make(view, Objects.requireNonNull(e.getMessage()), 3000).show();
             } catch (Exception e) {
                 Log.e(TAG, "onCreate: ", e);
                 Snackbar.make(view, "Something went wrong adding student", 3000).show();
             }
         });
 
+        findViewById(R.id.cancelButton).setOnClickListener(view -> {
+            super.finish();
+        });
+    }
+
+    private void validateFields() throws StudentTrackerException{
+        if (Strings.isEmptyOrWhitespace(firstName.getText().toString()) ||
+                Strings.isEmptyOrWhitespace(lastName.getText().toString()) ||
+                Strings.isEmptyOrWhitespace(phoneNumber.getText().toString()) ||
+                Strings.isEmptyOrWhitespace(dateOfBirth.getText().toString())) {
+            throw new StudentTrackerException("Fields cannot be empty");
+        }
+
     }
 
     private LocalDate parseDate() {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return LocalDate.parse(dateOfBirth.getText().toString(), dtf);
+        try {
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            return LocalDate.parse(dateOfBirth.getText().toString(), dtf);
+        } catch (Exception e) {
+            throw new StudentTrackerException("Invalid birthday, use day month year.");
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/bruijnes/studenttracker/exception/StudentTrackerException.java
@@ -0,0 +1,8 @@
+package com.bruijnes.studenttracker.exception;
+
+public class StudentTrackerException extends RuntimeException {
+
+    public StudentTrackerException(String message) {
+        super(message);
+    }
+}
